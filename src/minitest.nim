9e06d488fb5937e56fe3be22e965e44423e7f5d5
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-13 16:27:36 +0200

    new: Add Nim library

---
+++ src/minitest.nim
@@ -0,0 +1,11 @@
+#:__________________________________________________________________
+#  lex  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+#:__________________________________________________________________
+# @deps std
+import std/unittest ; export unittest
+unittest.abortOnError = true
+template describe *(name :string; body :untyped) :untyped= unittest.suite(name, body)
+template it       *(name :string; body :untyped) :untyped= unittest.test(name, body)
+template expect   *(cond :untyped) :untyped= unittest.check(cond)
+# TODO: Remove the std/unittest dependency
+
